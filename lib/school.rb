e7fe4e65b9e9dbc48402dc8b57784cb7f9da91f7
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-08 03:38:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ class School
         ROSTER[grade] ||= []
         ROSTER[grade] << name
       end
-
+        #if roster has a "grade" key push name to roster-grade, else "||=" - or equal to new array
           #   if ROSTER.has_key?[grade]
           #     ROSTER[grade] << name
           #   else
