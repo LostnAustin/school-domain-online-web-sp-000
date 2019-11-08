52af523a60863b4ec5f764bac783ff3a7f7707eb
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-08 02:24:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  ROSTER = Hash.new
+
+  def initialize(name)
+    @name = name
+    @grade = grade
+  @roster = ROSTER
+  end
+      def name
+        @name
+      end
+
+        def roster
+          @roster
+        end
+
+          def add_student(name, grade)
+              ROSTER["name"] = []
+              ROSTER["name"] = ["#{name}"]
+              ROSTER["grade"] = []
+                ROSTER["grade"] = "#{grade}"
+              ROSTER.flatten
+          end
+
+            def grade
+              @grade
+            end
+end
+#school = School.new(ROSTER)
