844211e34dc55ce0871c81f5188cf464471aeb7e
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-08 02:52:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,8 +15,8 @@ class School
         end
 
           def add_student(name, grade)
-             ROSTER[:name] = []
-             ROSTER[:name] << ["#{name}"]
+             ROSTER[name] = []
+             ROSTER[name] << ["#{name}"]
               ROSTER[:grade] = []
               ROSTER[:grade] = "#{grade}"
             #    ROSTER[:grade] << "#{name}"
